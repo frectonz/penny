e33e7d3f0ba20e7b904f87b3996fb5d0076a0a2b
frectonz <fraol0912@gmail.com>
2026-02-07 16:29:20 +0300

    feat: working nix build of penny

--- flake.nix
+++ flake.nix
@@ -53,10 +53,91 @@
         };
       });
 
+      packages = forAllSystems (
+        pkgs:
+        let
+          rustPlatform = pkgs.makeRustPlatform {
+            cargo = pkgs.rust-bin.stable.latest.default;
+            rustc = pkgs.rust-bin.stable.latest.default;
+          };
+
+          pname = "penny";
+          version = "0.0.12";
+        in
+        rec {
+          ui = pkgs.stdenvNoCC.mkDerivation {
+            pname = "${pname}-ui";
+            version = version;
+
+            src = ./ui;
+
+            nativeBuildInputs = [
+              pkgs.nodejs
+              pkgs.pnpmConfigHook
+              pkgs.pnpm
+            ];
+
+            pnpmDeps = pkgs.fetchPnpmDeps {
+              inherit pname version;
+              src = ./ui;
+              pnpm = pkgs.pnpm;
+              fetcherVersion = 3;
+              hash = "sha256-lE3HeKgyWUmD4IXtj4RxwjFfFv371bjl+0U2s/Zj3SE=";
+            };
+
+            buildPhase = ''
+              runHook preBuild
+
+              pnpm build
+
+              runHook postBuild
+            '';
+
+            installPhase = ''
+              runHook preInstall
+
+              cp -r dist $out
+              echo -n "v${version}" > $out/VERSION
+
+              runHook postInstall
+            '';
+          };
+
+          default = rustPlatform.buildRustPackage {
+            inherit pname version;
+            src = self;
+            buildInputs = [ pkgs.openssl ];
+            nativeBuildInputs = [
+              pkgs.pkg-config
+              pkgs.perl
+              pkgs.cmake
+            ];
+            cargoLock.lockFile = ./Cargo.lock;
+
+            preBuild = ''
+              cp -pr --reflink=auto -- ${ui} ui/dist
+            '';
+          };
+
+          image = pkgs.dockerTools.buildLayeredImage {
+            name = pname;
+            tag = "latest";
+            created = "now";
+            contents = [ default ];
+            config.ENTRYPOINT = [ "/bin/penny" ];
+          };
+
+          deploy = pkgs.writeShellScriptBin "deploy" ''
+            ${pkgs.skopeo}/bin/skopeo --insecure-policy copy docker-archive:${image} docker://docker.io/frectonz/penny:${version} --dest-creds="frectonz:$ACCESS_TOKEN"
+            ${pkgs.skopeo}/bin/skopeo --insecure-policy copy docker://docker.io/frectonz/penny:${version} docker://docker.io/frectonz/penny:latest --dest-creds="frectonz:$ACCESS_TOKEN"
+          '';
+        }
+      );
+
       formatter = forAllSystems (
         pkgs:
         pkgs.treefmt.withConfig {
-          runtimeInputs = [ pkgs.nixfmt-rfc-style ];
+          runtimeInputs = [ pkgs.nixfmt ];
           settings = {
             on-unmatched = "info";
             formatter.nixfmt = {
