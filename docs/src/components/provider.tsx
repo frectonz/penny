685d771b1cb07d91384fec08e8cb1669ab2580ea
Yoftahe <jofab08@gmail.com>
2026-03-28 19:37:11 +0300

    Add search functionality to docs

--- docs/src/components/provider.tsx
+++ docs/src/components/provider.tsx
@@ -1,12 +1,13 @@
 "use client";
 import { RootProvider } from "fumadocs-ui/provider/next";
 import type { ReactNode } from "react";
+import SearchDialog from "@/components/search-dialog";
 
 export function Provider({ children }: { children: ReactNode }) {
   return (
     <RootProvider
       search={{
-        enabled: false,
+        SearchDialog,
       }}
       theme={{ defaultTheme: "system" }}
     >
